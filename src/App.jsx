7923c022014865b9b076953acb73b102e6f83ce9
devsilva <jerson.silva99@hotmail.com>
2022-11-10 13:21:06 -0500

    feat: Import containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import { Work, About, Header, Footer, Skills, Testimonial, NavBar } from './utils/import'
+
+import './App.scss'
+
 const App = () => {
-  return <>
-  
-  </>
+  return <div className='app'>
+    <NavBar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
 }
 
 export default App
