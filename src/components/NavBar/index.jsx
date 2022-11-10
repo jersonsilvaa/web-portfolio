0ec9bc088f142755e3784bae6e5c31a6e1471823
devsilva <jerson.silva99@hotmail.com>
2022-11-10 13:42:47 -0500

    refactor: Map in the navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
+import { images } from '../../constants'
+
+import './styles.scss'
 const NavBar = () => {
     return <>
         <nav>
             <div>
                 <img src={images.logo} alt="logo" />
+                <ul>
+                    { ['inicio', 'sobre mi', 'trabajos', 'habilidades', 'contacto'].map((item) => (
+                        <li key={`link-${item}`}>
+                            <div />
+                            <a href={`#${item}`}>{item}</a>
+                        </li>
+                    )) }
+                </ul>
             </div>
         </nav>
     </>
