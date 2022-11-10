e388582ffcb1ab1ad3b42fbe8c4db1c490ec7514
devsilva <jerson.silva99@hotmail.com>
2022-11-10 13:25:14 -0500

    feat: Export container

---
+++ src/utils/import.js
@@ -0,0 +1,10 @@
+// export container
+export { default as Work } from '../container/Work'
+export { default as About } from '../container/About'
+export { default as Header } from '../container/Header'
+export { default as Footer } from '../container/Footer'
+export { default as Skills } from '../container/Skills'
+export { default as Testimonial } from '../container/Testimonial'
+
+// export component
+export { default as NavBar } from '../components/NavBar'
