14ea927b0a76137e1abacc86725b74064abada44
devsilva <jerson.silva99@hotmail.com>
2022-11-10 13:23:57 -0500

    feat: Add container navbar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+    return <>
+        <nav>
+            <div>
+                <img src={images.logo} alt="logo" />
+            </div>
+        </nav>
+    </>
+}
+
+export default NavBar
